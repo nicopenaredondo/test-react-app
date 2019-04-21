6cf20c13fd60e85e5a0b616ad1bc7b8cb648a1d0
Nico R. Penaredondo <npenaredondo@gmail.com>
2019-04-21 20:52:08 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Test
+            Test 2
           </a>
         </header>
       </div>
